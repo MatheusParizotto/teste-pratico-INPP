7b789b0652ee53986cfa741cc4699e976eaf86e4
Matheus Parizotto <matheusparizotto29@gmail.com>
2026-05-05 20:31:10 -0400

    feat: adicionando carregamento e tratamento de dados ausentes

--- script.py
+++ script.py
@@ -17,3 +17,36 @@ def verificar_arquivo(caminho: str):
             print("-", f)
 
         sys.exit(1)
+
+def carregar_dados(caminho_arquivo: str) -> pd.DataFrame:
+
+    df = pd.read_excel(caminho_arquivo)
+
+    # Converte coluna de data para datetime 
+    df["data"] = pd.to_datetime(df["data"])
+    print(df["data"].head())
+
+    return df
+
+
+def tratar_dados(df: pd.DataFrame) -> pd.DataFrame:
+
+    # Esse trecho trata os valores ausentes nas colunas que possuem dados incompletos
+    df["nivel_rio_m"] = df["nivel_rio_m"].interpolate()
+    df["ndvi"] = df["ndvi"].interpolate()
+    print("Rodou")
+
+    return df
+
+
+def main():
+    caminho = "dados.xlsx"
+
+    verificar_arquivo(caminho)
+
+    df = carregar_dados(caminho)
+    df = tratar_dados(df)
+
+
+if __name__ == "__main__":
+    main()
